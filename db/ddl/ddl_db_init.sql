347a75c7f61d57a72379de35defac2ae7c549216
john <john@john-PC>
2015-03-11 21:06:06 +0800

    修改数据库的部分表结构初始化字典表和分类表

--- db/ddl/ddl_db_init.sql
+++ db/ddl/ddl_db_init.sql
@@ -30,7 +30,7 @@ USE `dixin`;
 CREATE TABLE IF NOT EXISTS `area` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
   `name` varchar(64) NOT NULL COMMENT '地区名称',
-  `pid` int(11) NOT NULL COMMENT '父节点ID',
+  `pid` int(11) NOT NULL DEFAULT '-1' COMMENT '父节点ID',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `area` (
 CREATE TABLE IF NOT EXISTS `assignment` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
   `userid` int(11) NOT NULL COMMENT '转让用户',
-  `date` date NOT NULL COMMENT '转让日期',
+  `assigndate` date NOT NULL COMMENT '转让日期',
   `amount` double NOT NULL COMMENT '转让金额',
   `productid` int(11) NOT NULL COMMENT '转让产品',
   `paytype` int(11) NOT NULL COMMENT '付息方式',
@@ -61,12 +61,12 @@ CREATE TABLE IF NOT EXISTS `assignment` (
 
 CREATE TABLE IF NOT EXISTS `calllogs` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
-  `datetime` datetime NOT NULL COMMENT '联系日期时间',
+  `calltime` datetime NOT NULL COMMENT '联系日期时间',
   `callfrom` int(11) NOT NULL COMMENT '来电用户',
   `callto` int(11) NOT NULL COMMENT '去电用户',
-  `msg` varchar(200) NOT NULL COMMENT '通话信息',
+  `msg` varchar(1024) NOT NULL COMMENT '通话信息',
   PRIMARY KEY (`id`),
-  KEY `datetime` (`datetime`,`callfrom`,`callto`)
+  KEY `datetime` (`calltime`,`callfrom`,`callto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -78,11 +78,52 @@ CREATE TABLE IF NOT EXISTS `calllogs` (
 CREATE TABLE IF NOT EXISTS `catogry` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
   `name` varchar(64) NOT NULL COMMENT '产品分类名称',
-  `type` tinyint(4) NOT NULL COMMENT '分类类型，0：产品分类，1：金融机构分类，2：金融产品分类，3：资金投向，4：分红方式，5：问题分类，6：利益分配方式，7：权限分类',
+  `type` tinyint(4) NOT NULL COMMENT '分类类型，根据字典表',
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
   KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 转存表中的数据 `catogry`,预先设置的分类
+--
+
+INSERT INTO `catogry` (`id`, `name`, `type`) VALUES
+(1, '游客', 1),
+(10, '注册用户', 1),
+(20, '认证用户', 1),
+(25, '后台用户', 1),
+(30, '后台超级管理员', 1),
+(31, '债券', 2),
+(32, '银行理财', 2),
+(33, '基金', 2),
+(34, '券商理财', 2),
+(35, '信托', 2),
+(36, '资管', 2),
+(42, '固定收益', 3),
+(43, '浮动收益', 3);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `dict`
+--
+
+CREATE TABLE IF NOT EXISTS `dict` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
+  `name` varchar(64) NOT NULL COMMENT '产品分类名称 ',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 转存表中的数据 `dict`
+--
+
+INSERT INTO `dict` (`id`, `name`) VALUES
+(1, '用户权限分类'),
+(2, '产品分类'),
+(3, '收益分类'),
+(4, '留言问题分类');
 
 -- --------------------------------------------------------
 
@@ -128,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `userid` int(11) NOT NULL COMMENT '用户',
   `msgtime` datetime NOT NULL COMMENT '留言日期',
   `catogryid` int(11) NOT NULL COMMENT '问题类别',
-  `msg` varchar(256) NOT NULL COMMENT '留言',
+  `msg` varchar(1024) NOT NULL COMMENT '留言',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -157,10 +198,12 @@ CREATE TABLE IF NOT EXISTS `product` (
   `name` varchar(128) NOT NULL COMMENT '产品名称',
   `release_date` date NOT NULL COMMENT '发行时间',
   `term` int(11) NOT NULL COMMENT '期限',
-  `percent_a` float NOT NULL COMMENT 'A类份额',
+  `part_a` int(11) NOT NULL COMMENT 'A类份额',
   `rate_a` float NOT NULL COMMENT 'A类年华收益率',
-  `percent_b` float NOT NULL COMMENT 'B类份额',
+  `part_b` int(11) NOT NULL COMMENT 'B类份额',
   `rate_b` float NOT NULL COMMENT 'B类年华收益率',
+  `part_c` int(11) NOT NULL COMMENT 'C类份额',
+  `rate_c` float NOT NULL COMMENT 'C类年华收益率',
   `amount` double NOT NULL COMMENT '产品规模',
   `mina_mount` double NOT NULL COMMENT '投资起点',
   `pay_type` int(11) NOT NULL COMMENT '利益分配方式',
@@ -186,13 +229,13 @@ CREATE TABLE IF NOT EXISTS `product` (
 CREATE TABLE IF NOT EXISTS `purchase` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
   `userid` int(11) NOT NULL COMMENT '购买用户',
-  `date` date NOT NULL COMMENT '购买日期',
+  `buydate` date NOT NULL COMMENT '购买日期',
   `productid` int(11) NOT NULL COMMENT '购买产品',
   `volume` int(11) NOT NULL COMMENT '购买数量',
   `amount` int(11) NOT NULL COMMENT '购买金额',
   `fiid` int(11) NOT NULL COMMENT '营销机构',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -201,14 +244,15 @@ CREATE TABLE IF NOT EXISTS `purchase` (
 --
 
 CREATE TABLE IF NOT EXISTS `reservation` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部唯一ID',
   `userid` int(11) NOT NULL COMMENT '预约用户',
-  `date` date NOT NULL COMMENT '预约日期',
+  `reservedate` date NOT NULL COMMENT '预约日期',
   `productid` int(11) NOT NULL COMMENT '预约产品',
   `amount` double NOT NULL COMMENT '预约金额',
-  `msg` varchar(256) NOT NULL COMMENT '预约留言',
+  `msg` varchar(1024) NOT NULL COMMENT '预约留言',
+  PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -222,19 +266,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(64) NOT NULL COMMENT '密码',
   `type` int(11) NOT NULL COMMENT '是客户还是后台管理用户',
   `enabled` tinyint(1) NOT NULL COMMENT '是否有效',
-  `date` date NOT NULL COMMENT '注册日期',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `userinfo`用户信息表
---
-
-CREATE TABLE IF NOT EXISTS `userinfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内部ID',
-  `userid` int(11) NOT NULL COMMENT '关联用户ID',
+  `regdate` date NOT NULL COMMENT '注册日期',
   `name` int(11) NOT NULL COMMENT '姓名',
   `mobile` varchar(11) NOT NULL COMMENT '手机号',
   `areaid` int(11) NOT NULL COMMENT '地区ID',
