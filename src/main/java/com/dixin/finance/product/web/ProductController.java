ec347e1d037e6e79606219708877d777e2d2469b
U-3XX4VY1\Administrator <3XX4VY1+Administrator@3XX4VY1.comtop.local>
2015-03-18 10:54:56 +0800

    删除多余import

--- src/main/java/com/dixin/finance/product/web/ProductController.java
+++ src/main/java/com/dixin/finance/product/web/ProductController.java
@@ -16,14 +16,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.dixin.finance.authentication.vo.UserVO;
 import com.dixin.finance.product.service.IProductService;
 import com.dixin.finance.product.vo.ProductVO;
 import com.dixin.framework.base.web.BaseWebResult;
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
 
 /**
  * @author Administrator
