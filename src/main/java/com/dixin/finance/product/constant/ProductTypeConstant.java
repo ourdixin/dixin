5748d689297b864cc509bf2d3189f9b4ad7681bd
john <john@john-PC>
2015-03-22 14:16:54 +0800

    添加缺少的文件

---
+++ src/main/java/com/dixin/finance/product/constant/ProductTypeConstant.java
@@ -0,0 +1,42 @@
+package com.dixin.finance.product.constant;
+
+
+public interface ProductTypeConstant {
+	
+	
+	/**
+	 * 所有
+	 */
+	final int ALL = 30;	
+	
+	/**
+	 * 债券
+	 */
+	final int BOND = 31;
+	
+	/**
+	 * 银行理财
+	 */
+	final int BANK_FINANCING = 32;
+	
+	/**
+	 * 基金
+	 */
+	final int FUND = 33;
+	
+	/**
+	 * 券商理财
+	 */
+	final int BROKERAGE_FINANCING = 34;
+	
+	/**
+	 * 信托
+	 */
+	final int TRUST = 35;
+	
+	/**
+	 * 资管
+	 */
+	final int ASSET_MANAGEMENT = 36;	
+	
+}
