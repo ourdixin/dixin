b299f8d384b5a2559bd0a5a27b82ebe34509e77f
john <john@john-PC>
2015-04-19 11:27:10 +0800

    修改数据表结构

--- db/ddl/ddl_db_init.sql
+++ db/ddl/ddl_db_init.sql
@@ -224,7 +224,7 @@ CREATE TABLE IF NOT EXISTS `financial_institution` (
 --
 
 CREATE TABLE IF NOT EXISTS `financial_manager` (
-  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  `id` int(11) NOT NULL COMMENT '内部唯一ID',
   `info` varchar(256) NOT NULL COMMENT '专属财务经理介绍',
   `title` varchar(64) NOT NULL COMMENT '职务',
   `education` varchar(64) NOT NULL COMMENT '学历',
@@ -235,7 +235,7 @@ CREATE TABLE IF NOT EXISTS `financial_manager` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_user` int(11) NOT NULL COMMENT '更新人',
   `update_time` datetime NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -388,11 +388,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `start_date` date COMMENT '认证开始日期',
   `term` int(11) COMMENT '期限',
   `auth_type` int(11) COMMENT '认证类别',
+  `fm_id` int(11) NOT NULL COMMENT '财务经理ID',
   `create_user` int(11) NOT NULL COMMENT '创建人',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_user` int(11) NOT NULL COMMENT '更新人',
   `update_time` datetime NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fm_id` (`fm_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
